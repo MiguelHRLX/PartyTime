90cc3739ad28888af2fa7afb7a799300c38c4a15
MiguelHRLX <miguel.huamani.r@uni.pe>
2023-03-28 20:13:09 -0500

    main UI

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class UIManager : MonoBehaviour
 {
+    public Camera mainCamera;
+    public Canvas canvas;
     // Start is called before the first frame update
     void Start()
     {
         
     }
-
     // Update is called once per frame
     void Update()
     {
-        
+        canvas.transform.forward = mainCamera.transform.forward;
     }
 }
